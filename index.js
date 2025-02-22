6a7ff036ef14f5df20a9d1bbcbae439cbcddd70b
Anthony Frank A. Ordoñez <anthonyfrankaordonez@gmail.com>
2025-02-22 15:48:04 +0800

    Added Eslint config on Backend

--- index.js
+++ index.js
@@ -1,110 +1,105 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const mongoose = require("mongoose");
-const Note = require("./models/note");
+const express = require('express')
+const cors = require('cors')
+const app = express()
+// const mongoose = require("mongoose");
+const Note = require('./models/note')
 
-app.use(express.json());
-app.use(express.static("dist"));
-app.use(cors());
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
+    response.json(notes)
+  })
+})
 
-app.get("/api/notes/:id", (request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndUpdate(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-  });
+  })
 
   note
     .save()
     .then((savedNote) => {
-      response.json(savedNote);
+      response.json(savedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/notes/:id", (request, reponse, next) => {
-  const { content, important } = request.body;
-
-  // const note = {
-  //   content: content,
-  //   important: important,
-  // };
+app.put('/api/notes/:id', (request, reponse, next) => {
+  const { content, important } = request.body
 
   Note.findByIdAndUpdate(
     request.params.id,
     { content, important },
-    { new: true, runValidators: true, context: "query" }
+    { new: true, runValidators: true, context: 'query' }
   )
     .then((updatedNote) => {
-      reponse.json(updatedNote);
+      reponse.json(updatedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, reponse) => {
-  reponse.status(404).send({ error: "unknown endpoint" });
-};
+  reponse.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message);
+  console.log(error.message)
 
-  if (error.name === "CastError")
-    return response.status(400).send({ error: "malformed id" });
-  else if (error.name === "ValidationError")
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError')
+    return response.status(400).send({ error: 'malformed id' })
+  else if (error.name === 'ValidationError')
+    return response.status(400).json({ error: error.message })
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on the port http://localhost:${PORT}/`);
-});
+  console.log(`Server running on the port http://localhost:${PORT}/`)
+})
 
-/* 
+/*
 LEGACY CODE:
 
 let notes = [
   {
-    id: "1", 
+    id: "1",
     content: "HTML is easy",
     important: true,
   },
--- mongo.js
+++ mongo.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://afordonez:${password}@cluster0.d6mgi.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://afordonez:${password}@cluster0.d6mgi.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
 //   content: "Mongoose make thing easy",
@@ -29,9 +29,9 @@ const Note = mongoose.model("Note", noteSchema);
 //   mongoose.connection.close();
 // });
 
-Note.find({ content: "HTML" }).then((result) => {
+Note.find({ content: 'HTML' }).then((result) => {
   result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
